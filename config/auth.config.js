9a35c320d67deb58946b9429fac5779686f8ee7a
Tony Kosseify <tonykosseify123@gmail.com>
2023-07-23 12:06:23 +0300

    Confguring auth

---
+++ config/auth.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  secret: "bezkoder-secret-key",
+  jwtExpiration: 60 ,           // 1 min
+  jwtRefreshExpiration: 620,   // 24 
+
+  /* for test */
+  // jwtExpiration: 60,          // 1 minute
+  // jwtRefreshExpiration: 120,  // 2 minutes
+};
