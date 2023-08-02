3ca6aa774088c76bd838ba284cba323da301aa5b
Murf-y <charbelfayad64@gmail.com>
2023-08-02 13:40:26 +0300

    fix: add _ as a special character for password validation

--- validations/auth/loginSchema.js
+++ validations/auth/loginSchema.js
@@ -1,21 +1,29 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const isLAUEmail = (value, helpers) => {
-    if (value.endsWith('@lau.edu') || value.endsWith('@lau.edu.lb')) {
-        return value; 
-    }
-    return helpers.error('any.invalid');
+  if (value.endsWith("@lau.edu") || value.endsWith("@lau.edu.lb")) {
+    return value;
+  }
+  return helpers.error("any.invalid");
 };
 
 const loginSchema = Joi.object({
-    email: Joi.string().required().email().custom(isLAUEmail, 'Not a valid LAU email'),
-    password: Joi.string()
+  email: Joi.string()
     .required()
-    .min(8) 
+    .email()
+    .custom(isLAUEmail, "Not a valid LAU email"),
+  password: Joi.string()
+    .required()
+    .min(8)
     .max(50)
-    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$'))
-    .message('"{#label}" must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character.')
+    .pattern(
+      new RegExp(
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&_])[A-Za-z\\d@$!%*?&]+$"
+      )
+    )
+    .message(
+      '"{#label}" must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character.'
+    ),
 });
-
 
 module.exports = loginSchema;
