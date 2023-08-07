ec5536e49fe25589c142f6ca70c69f68bf4d0dc4
Jean-Paul Bassil <jean.paul.bassil@outlook.com>
2023-08-08 00:00:37 +0300

    added userEvent api validation

--- controllers/userEventsController.js
+++ controllers/userEventsController.js
@@ -1,7 +1,6 @@
 const { Event, Club, User, UserEvent } = require("../models");
 const defaultCruds = require("./defaultCruds.js");
 const respond = require("../utils/respond.js");
-const { postSchema } = require("../validations/userEvent");
 
 const getAll = defaultCruds.getAll(UserEvent)
 const getOne = defaultCruds.getOne(UserEvent)
@@ -11,12 +10,9 @@ const deleteOne = defaultCruds.deleteOne(UserEvent)
 
 const create = async (req, res, next) => {
     try {
-        const { error, value } = postSchema.validate(req.body);
-        const userId = req.userId;
-        if (error) {
-            return respond(res, 400, { message: error.message });
-        }
-        const { eventId, status } = value;
+        const userId = req.body.userId;
+        const eventId = req.body.eventId;
+        const status = req.body.status;
         const event = await Event.findByPk(eventId);
         if (!event) {
             return respond(res, 404, { message: "Event not found" });
