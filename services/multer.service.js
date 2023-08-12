41c3f86781ca070375e246c04c39822ef07b855f
Jean-Paul Bassil <jean.paul.bassil@outlook.com>
2023-08-12 15:03:19 +0300

    added imagePath to models

--- services/multer.service.js
+++ services/multer.service.js
@@ -1,17 +1,34 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../images/')
+    const dir = '../images/';
+
+    fs.access(dir, fs.constants.F_OK, (err) => {
+      if (err) {
+        return fs.mkdir(dir, { recursive: true }, (err) => {
+          cb(err, dir);
+        });
+      }
+      cb(null, dir);
+    });
   },
   filename: function (req, file, cb) {
-    const date = new Date().toISOString().replace(/:/g, '-');
     const extension = path.extname(file.originalname);
+    if (extension !== '.png' && extension !== '.jpg' && extension !== '.jpeg') {
+      return cb(new Error('Unsupported file type'), null);
+    }
+
+    const date = new Date().toISOString().replace(/:/g, '-');
     cb(null, `${file.fieldname}-${date}${extension}`);
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+}).single('file');
 
 module.exports = upload;
