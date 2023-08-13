2528b692009137ead380a453e8dbc60a4d99c5d6
Peter Chahid <peterdaaboul@outlook.com>
2023-08-13 18:30:01 +0300

    changes to default

--- controllers/defaultCruds.js
+++ controllers/defaultCruds.js
@@ -1,106 +1,113 @@
-const respond = require('../utils/respond.js')
-
+const respond = require("../utils/respond.js");
 
 const getAll = (Model) => {
-    return async (req, res, next) => {
-        try {
-            const output = await Model.findAll()
-            res && respond(res, 200, output)
-        } catch (err) {
-            next(err)
-        }
+  return async (req, res, next) => {
+    try {
+      const output = await Model.findAll();
+      res && respond(res, 200, output);
+    } catch (err) {
+      next(err);
     }
-}
+  };
+};
 
 const findOne = (Model) => {
-    return async (whereClause, res) => {
-      try {
-        const output = await Model.findOne({ where: whereClause });
-        if (!output) {
-          return respond(res, 404, { message: "Item not found" });
-        }
-        return output;
-      } catch (err) {
-        res && respond(res, 500, { message: err.message });
+  return async (whereClause, res) => {
+    try {
+      const output = await Model.findOne({ where: whereClause });
+      if (!output) {
+        return respond(res, 404, { message: "Item not found" });
       }
-    };
+      return output;
+    } catch (err) {
+      res &&
+        respond(res, 500, {
+          message: "An error occurred while retrieving the item.",
+        });
+    }
+  };
 };
 
 const getOne = (Model) => {
-    return async (req, res, next) => {
-        try {
-            const output = await Model.findByPk(req.params.id)
-            if (!output){
-                return respond(res, 404, {message: "Item not found"})
-            }
-            res && respond(res, 200, output)
-        } catch (err) {
-            next(err)
-        }
+  return async (req, res, next) => {
+    try {
+      const output = await Model.findByPk(req.params.id);
+      if (!output) {
+        return respond(res, 404, { message: "Item not found" });
+      }
+      res && respond(res, 200, output);
+    } catch (err) {
+      next(err);
     }
-}
+  };
+};
 
 const create = (Model, schema) => {
-    return async (req, res, next) => {
-        try {
-            if (schema){
-                const {error} = schema.validate(req.body);
-                if (error) {
-                    return respond(res, 400, error.details[0].message);
-                }
-            }
-
-            const output = await Model.create(req.body)
-            res && respond(res, 201, output)
-            return output
-        } catch (err) {
-            res && respond(res, 500, {message: err.message})
+  return async (req, res, next) => {
+    try {
+      if (schema) {
+        const { error } = schema.validate(req.body);
+        if (error) {
+          return respond(res, 400, error.details[0].message);
         }
+      }
+
+      const output = await Model.create(req.body);
+      res && respond(res, 201, output);
+      return output;
+    } catch (err) {
+      res &&
+        respond(res, 500, {
+          message: "An error occurred while creating the item.",
+        });
     }
-}
+  };
+};
 
 const update = (Model) => {
-    return async (req, res, next) => {
-        try {
-            let output;
-            if(req.body.where){
-                output = await Model.findOne({ where: req.body.where })
-            } else {
-                output = await Model.findByPk(req.params.id)
-            }
+  return async (req, res, next) => {
+    try {
+      let output;
+      if (req.body.where) {
+        output = await Model.findOne({ where: req.body.where });
+      } else {
+        output = await Model.findByPk(req.params.id);
+      }
 
-            if (!output){
-                return respond(res, 404, {message: "Item not found"})
-            }
-            await output.update(req.body)
-            return respond(res, 200, output)
-        } catch(err){
-            res && respond(res, 500, {message: err.message})
-        }
+      if (!output) {
+        return respond(res, 404, { message: "Item not found" });
+      }
+      await output.update(req.body);
+      return respond(res, 200, output);
+    } catch (err) {
+      res &&
+        respond(res, 500, {
+          message: "An error occurred while updating the item.",
+        });
     }
-}
-
+  };
+};
 
 const deleteOne = (Model) => {
-    return async (req, res, next) => {
-        try {
-          const output = await Model.findByPk(req.params.id);
-          if (!output) {
-            return respond(res, 404, { message: 'Item not found' })
-          }
-          await output.destroy();
-          res && respond(res, 204, { message: 'Item deleted successfully' });
-        } catch (err) {
-          next(err);
-        }
-      };
-}
+  return async (req, res, next) => {
+    try {
+      const output = await Model.findByPk(req.params.id);
+      if (!output) {
+        return respond(res, 404, { message: "Item not found" });
+      }
+      await output.destroy();
+      res && respond(res, 204, { message: "Item deleted successfully" });
+    } catch (err) {
+      next(err);
+    }
+  };
+};
 
 module.exports = {
-    getAll,
-    getOne,
-    create,
-    update,
-    deleteOne,
-    findOne
-}
+  getAll,
+  getOne,
+  create,
+  update,
+  deleteOne,
+  findOne,
+};
